495e994326f5b24b60b26e6853f9ec7c23328cf6
Michael <michael.martins@hotmail.fr>
2020-06-05 10:19:04 +0200

    First commit

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailsNeighbourActivity.java
@@ -0,0 +1,16 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.openclassrooms.entrevoisins.R;
+
+public class DetailsNeighbourActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details_neighbour_activity);
+    }
+}
